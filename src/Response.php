588fe4dc4d6841c99eb28e3027295f052cb16f65
義鈞 <wilkques@MacBook-Pro.local>
2021-07-14 14:58:28 +0800

    add response interface JsonSerializable, ArrayAccess

--- src/Response.php
+++ src/Response.php
@@ -2,12 +2,14 @@
 
 namespace Wilkques\HttpClient;
 
+use ArrayAccess;
+use JsonSerializable;
 use Wilkques\HttpClient\Exception\RequestException;
 
 /**
  * A class represents API response.
  */
-class Response
+class Response implements JsonSerializable, ArrayAccess
 {
     /** @var int */
     private $httpStatus;
@@ -297,5 +299,109 @@ class Response
     public function __toString()
     {
         return $this->body();
+    }
+
+    /**
+     * Convert the object into something JSON serializable.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return $this->json();
+    }
+
+    /**
+     * Get the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return $this->json()[$offset];
+    }
+
+    /**
+     * Set the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->json()[$offset] = $value;
+    }
+
+    /**
+     * Determine if the given attribute exists.
+     *
+     * @param  mixed  $offset
+     * @return bool
+     */
+    public function offsetExists($offset)
+    {
+        return !array_key_exists($offset, $this->json()) && !is_null($this->json()[$offset]);
+    }
+
+    /**
+     * Unset the value for a given offset.
+     *
+     * @param  mixed  $offset
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->json()[$offset]);
+    }
+
+    /**
+     * Determine if an attribute or relation exists on the model.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function __isset($key)
+    {
+        return $this->offsetExists($key);
+    }
+
+    /**
+     * Unset an attribute on the model.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function __unset($key)
+    {
+        $this->offsetUnset($key);
+    }
+
+    /**
+     * @param string $key
+     * 
+     * @return mixed
+     */
+    public function __get(string $key)
+    {
+        if (property_exists($this, $key)) {
+            return $this->{$key};
+        }
+
+        return $this->json()[$key];
+    }
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     */
+    public function __set(string $key, $value)
+    {
+        if (property_exists($this, $key)) {
+            $this->{$key} = $value;
+        } else {
+            $this->json()[$key] = $value;
+        }
     }
 }
